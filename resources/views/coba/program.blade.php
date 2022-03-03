3c6cd1bd15ba15d16e50a003a4b22054ce74be85
Tzelginia NC <49942388+Tzelginia@users.noreply.github.com>
2022-03-03 08:30:00 +0700

    penambahan view product dan program

---
+++ resources/views/coba/program.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Home</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+<div class="jumbotron">
+  <div class="container">
+    <h1 class="text-center"><b>Selamat Datang Di Website Saya</h1> </b>    
+  </div>
+</div>
+
+<div class="card bg-warning">
+    <div class="card-body">
+      <p class="card-text text-center"><b>Program</p></b>
+      <ul>
+          <li>Karir</li>
+          <li>Magang</li>
+          <li>Kunjungan Industri</li>
+      </ul>
+    </div>
+  </div>
+</body>
+</html>
